3ae169d86673236987089e663ae6c8be0091ad06
freyaliesel <80221739+freyaliesel@users.noreply.github.com>
2022-06-03 13:07:34 -0500

    outlined routes

---
+++ routes/api/thoughtRoutes.js
@@ -0,0 +1,19 @@
+// default route: /api/thoughts
+
+// GET all thoughts
+
+// GET single thought by _id
+
+// POST new thought - and push thought's _id to the users's thought array
+
+// PUT to update a thought by _id
+
+// DELETE a thought by _id
+
+
+
+// reaction route - /api/thoughts/:thoughtId/reactions
+
+// POST a reaction stored in a thought's reactions array
+
+// DELETE to pull and remove a reaction by reactionId
---
+++ routes/api/userRoutes.js
@@ -0,0 +1,21 @@
+
+// default route: /api/users
+
+// GET all users
+
+// GET single user by _id, populate thought and friend data
+
+// POST a new user
+
+// PUT to update a user by _id
+
+// DELETE to remove user by _id
+// BONUS - remove users associated thoughts when deleted
+
+
+
+// api/users/:userId/friends/:friendId
+
+// POST a new friend to users friend list
+
+// DELETE a friend from users friend list
