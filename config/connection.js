cbddea293e4823228b4cac6f73a5a21c9e4b6d77
freyaliesel <80221739+freyaliesel@users.noreply.github.com>
2022-06-06 14:05:46 -0500

    changed name of database

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,6 @@
 const {connect, connection} = require("mongoose");
 
-connect("mongodb://localhost:27017/mygroceryDB", {
+connect("mongodb://localhost:27017/socialNetworkDB", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 });
