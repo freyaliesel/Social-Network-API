ec30cbe3659ec0cae3c4da2689091645fcc237a2
freyaliesel <80221739+freyaliesel@users.noreply.github.com>
2022-06-08 12:45:00 -0500

    no seeds

--- controllers/userController.js
+++ controllers/userController.js
@@ -66,11 +66,6 @@ async function deleteUser(req, res) {
         const thoughts = await Thought.deleteMany({
             _id: { $in: user.thoughts },
         });
-        // // remove references to deleted user
-        // await User.update(
-        //     { friends: req.params.userId },
-        //     { $pull: { friends: req.params.userId } }
-        // );
         res.status(201).json({ message: "User deleted", user });
     } catch (err) {
         res.status(500).json(err);
